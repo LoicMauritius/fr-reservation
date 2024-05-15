d2946bb25fa51f8cd2998d6f742c41e55bb764e3
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-05-15 10:45:08 +0200

    chore: Update _app.js to conditionally render Compte component based on Component name

--- src/app/_app.js
+++ src/app/_app.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { AuthProvider } from "./context/authContext";
+import Compte from "./app/compte/page"; // importez votre composant Compte
 
 function MyApp({ Component, pageProps }) {
     return (
         <AuthProvider>
-            <Component {...pageProps} />
+            {Component.name === 'Compte' ? <Compte {...pageProps} /> : <Component {...pageProps} />}
         </AuthProvider>
     );
 }
