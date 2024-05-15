634efedaf3ef8f1b4ec915dd2292bcc137d64f3e
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-05-15 10:38:41 +0200

    chore: Update compte page to display login and registration forms based on user authentication status

--- src/app/compte/page.tsx
+++ src/app/compte/page.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import React from "react";
-import "@/styles/animations.css";
+import { useAuth } from "../context/authContext.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import "@/styles/animations.css";
 
 const Compte = () => {
+    const { isLoggedIn } = useAuth();
 
-    return(
-        <>
-            <h2>Compte <FontAwesomeIcon icon={faUser} className="rotate" /></h2>
-        </>
+    return (
+        <div>
+            <h2>Compte <FontAwesomeIcon icon={faUser} className="rotate"/></h2>
+            {isLoggedIn ? (
+                <p>Vous êtes connecté</p>
+            ) : (
+                <div>
+                    <p>Formulaire d'inscription</p>
+                    <p>Formulaire de connexion</p>
+                </div>
+            )}
+        </div>
     );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import React from 'react';
 
 const Accueil = () => {
 
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+
+        </div>
+    );
 };
 
 export default Accueil;
