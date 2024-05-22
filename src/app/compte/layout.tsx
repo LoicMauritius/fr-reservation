e98f89c8494cbbd4c5d1f435033d0f1598ed6217
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-05-22 13:39:54 +0200

    Update layout.tsx

--- src/app/compte/layout.tsx
+++ src/app/compte/layout.tsx
@@ -1,5 +1,7 @@
 import "@/styles/compte.css";
 
+import React from "react";
+
 function CompteLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
     return (
         <section className="compte">
