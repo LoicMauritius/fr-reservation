addf237c87537f986a56c8a2e46e5c27a1666c45
LoicMauritius <loic.mauritius@gmail.com>
2024-05-31 21:24:58 +0200

    Ajout du panier v1 - voir les reservations

---
+++ src/app/(main)/panier/page.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import { useUser } from '@/context/UserProvider';
+import { TrainType } from '@/db/model/Train';
+import { addOption, getReservations, getTrainbyReservations } from '@/db/reservations/ReservationActions';
+import Image from 'next/image';
+import Logo from '@/img/header/logo.png';
+import Fleche from '@/img/utils/fleche.png';
+import { bebasNeue } from '@/style/font';
+import { useEffect, useState } from 'react';
+import { Reservation, ReservationType } from '@/db/model/Reservation';
+import '@/style/reservation.css';
+
+interface TrainRidesProps{
+    reservations: ReservationType[],
+    data: TrainType[]
+}
+
+function TrainRides({reservations,data} : TrainRidesProps) {
+    const { user } = useUser();
+    const [persons, setPersons] = useState(0);
+
+    async function handleSubmit(data:FormData){
+        //AddOptions or delete
+    }
+
+    return (
+        <section className='TrainRides'>
+            {   //Display all the Train rides on the database
+                <>
+                    {data.map((TrainRide: TrainType, index: number) => (
+                        <>
+                            <form action={handleSubmit} key={`Train - ${TrainRide.id_train} ${index}`} className='rideInfos'>
+
+                                <input type="hidden" name="id_User" value={user?.id_User}/>
+                                <input type="hidden" name="id_train" value={TrainRide.id_train.toFixed(0)}/>
+                                <input type="hidden" name="price" value={TrainRide.price}/>
+                                <input type="hidden" name="persons" value={persons}/>
+
+                                <div className='ticketHeader'>
+                                    <div id='logo'>
+                                        <Image src={Logo} alt='logo' />
+                                    </div>
+                                    
+                                    <div id='stations'>
+                                        <h2 className={bebasNeue.className}>{TrainRide.gare_depart}</h2>
+                                        <Image src={Fleche} alt='fleche' />
+                                        <h2 className={bebasNeue.className}>{TrainRide.gare_arrive}</h2>
+                                    </div>
+                                </div>
+
+                                <div className='ticketInformations'>
+                                    <div className='infos'>
+                                        <div id='topInfos'>
+                                            <div>
+                                                <h2 className={bebasNeue.className}>De</h2>
+                                                <p>{TrainRide.gare_depart}</p>
+                                                <h2 className={bebasNeue.className}>Vers</h2>
+                                                <p>{TrainRide.gare_arrive}</p>
+                                            </div>
+                                            <div>
+                                                <h2 className={bebasNeue.className}>le</h2>
+                                                <p>{TrainRide.date_depart.toLocaleDateString()}</p>
+                                                <h2 className={bebasNeue.className}>Jusqu'au</h2>
+                                                <p>{TrainRide.date_arrive.toLocaleDateString()}</p>
+                                            </div>
+                                        </div>
+                                        <div id='bottomInfos'>
+                                            <div>
+                                                <h2 className={bebasNeue.className}>Nombre de places restantes</h2>
+                                                <p>{TrainRide.nb_place_dispo.toString()}</p>
+                                            </div>
+                                            <h2 className={bebasNeue.className}>{TrainRide.aller_retour ? "Aller-Retour" : "Aller Simple"}</h2>
+                                        </div>
+                                    </div>
+
+                                    <section className='bookTicket'>
+                                        <div>
+                                            <h2 className={bebasNeue.className}>Nom / prenom</h2>
+                                            <p>{user ? user.name.last_name + ' ' + user.name.first_name : "Pas d'utilisateur renseigné"}</p>
+                                        </div>
+                                        <div>
+                                            <h2 className={bebasNeue.className}>Nombre de personnes</h2>
+                                            <input value={persons} onChange={(e) => setPersons(parseInt(e.target.value))} placeholder='Nombre de personnes...' type="number" name="nbPersons" id="nb" required />
+                                        </div>
+                                        <div>
+                                            <h2 className={bebasNeue.className}>Prix à l'unité</h2>
+                                            <p>{TrainRide.price}</p>
+                                        </div>
+                                        
+                                    </section>
+                                </div>
+                                <div className="submit">
+                                    <input type="submit" value="Supprimer" />
+                                </div>
+                            </form>
+                        </>
+                    ))}
+                </>}
+        </section>
+    );
+}
+
+export default function Panier(){
+
+    const { user } = useUser();
+    const [reservations, setReservations] = useState<ReservationType[]>([]);
+    const [trains, setTrains] = useState<TrainType[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try{
+                console.log('user: ',user);
+                if(user){
+                    const datas = await getReservations(user.id_User);
+                    console.log(datas)
+                    if(datas && datas.length > 0){
+                        const reservationsList = datas;
+                        setReservations(reservationsList);
+                    }
+                }
+                
+            }catch(err){
+
+            }
+        }
+        
+        fetchData();
+    },[]);
+
+    useEffect(() => {
+        async function fetchTrains() {
+            try {
+                console.log('reserv: ',reservations);
+                for (const reserv of reservations) {
+                    console.log('fetch train n ',reserv.id_train)
+                    const train = await getTrainbyReservations(reserv.id_train).then((train) => train);
+                    
+                    console.log('train',train); 
+
+                    if(train){
+                        setTrains([...trains, train])
+                    }
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        
+        fetchTrains();
+    },[reservations]);
+
+    return(
+        <>
+            <form action={""}>
+                { trains ? <TrainRides reservations={reservations} data={trains}/> : <></>}
+            </form>
+        </>
+    )
+}
--- src/db/reservations/ReservationActions.ts
+++ src/db/reservations/ReservationActions.ts
@@ -2,9 +2,9 @@
 
 import { Model } from "mongoose";
 import { connectToDB } from "../cnx";
-import getTrainModel, { Train } from "../model/Train";
+import getTrainModel, { Train, TrainType } from "../model/Train";
 import { v4 as uuidv4 } from "uuid";
-import getReservationModel, { Reservation } from "../model/Reservation";
+import getReservationModel, { Reservation, ReservationType } from "../model/Reservation";
 
 export const allTrainRide = async () => {
     connectToDB();
@@ -51,3 +51,98 @@ export const reserveTrain = async (data: FormData) =>{
     
     return false;
 }
+
+export const addOption = async (data: FormData) =>{
+
+    const id_Reservation = data.get('id_Reservation') as string;
+    const id_Option = data.get('id_Option') as string;
+    const price = parseInt(data.get('total_price') as string);
+
+    try {
+        const ReservationModel = getReservationModel() as Model<Reservation>;
+
+        // Trouver la réservation existante
+        const existingReservation = await ReservationModel.findOne({
+            id_Reservation: id_Reservation,
+        });
+
+        if (!existingReservation) {
+            return false;
+        }
+
+        // Ajouter le nouvel id d'option à la liste existante
+        const newIdOptions = [...existingReservation.idOptions, id_Option];
+
+        // Calculer le nouveau prix total
+        const newTotalPrice = existingReservation.total_price + price;
+
+        // Mettre à jour la réservation existante avec les nouvelles valeurs
+        const updatedReservation = await ReservationModel.updateOne({
+            id_Reservation: id_Reservation,
+        }, {
+            $set: { idOptions: newIdOptions, total_price: newTotalPrice }
+        });
+
+        return true;
+
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+export const getReservations = async (id_User: string) =>{
+
+    try{
+        const reservationsList:ReservationType[] = [];
+        const ReservationModel = getReservationModel() as Model<Reservation>;
+
+        //Récupère les reservatiobs de l'utilisateur
+        const reservations = await ReservationModel.find({
+            id_User: id_User
+        }).lean();
+        console.log('reservations: ',reservations)
+
+        reservations.map((value) => reservationsList.push(value as Reservation as ReservationType))
+        console.log('reservations: ',reservationsList)
+
+        if(reservations.length > 0) return reservationsList;
+
+        return null;
+    }catch(err){
+        console.error(err)
+        return null;
+    }
+    
+}
+
+export const getTrainbyReservations = async (id_Train: number) => {
+    try {
+        const TrainModel = getTrainModel() as Model<Train>;
+        console.log(id_Train)
+    
+        const TrainRides = await TrainModel.findOne({
+            id_train: id_Train,
+        });
+        console.log('TrainRides: ',TrainRides)
+  
+        if (TrainRides) {
+            const train: TrainType = {
+            id_train: TrainRides.id_train,
+            gare_depart: TrainRides.gare_depart,
+            gare_arrive: TrainRides.gare_arrive,
+            date_depart: TrainRides.date_depart,
+            date_arrive: TrainRides.date_arrive,
+            aller_retour: TrainRides.aller_retour,
+            max_place: TrainRides.max_place,
+            nb_place_dispo: TrainRides.nb_place_dispo,
+            price: TrainRides.price,
+            };
+            console.log('train: ',train)
+            return train;
+        }
+        return null;
+    } catch (err) {
+        console.log(err);
+    }
+};
